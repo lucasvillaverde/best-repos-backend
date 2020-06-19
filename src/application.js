70efd54c25c3d83b46c2656140fffbd5100026d7
Lucas Villa Verde <lucasgabrielvverde@gmail.com>
2020-06-18 21:27:20 -0300

    Configured CORS

--- src/application.js
+++ src/application.js
@@ -4,11 +4,15 @@ const routes = require('./routes');
 const cors = require('cors');
 require('./database');
 
+var corsOptions = {
+    origin: 'http://best-repos.herokuapp.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
+app.use(routes);
 app.use(express.json({
     limit: '4MB'
 }));
-app.use(cors());
-
-app.use(routes);
 
 module.exports = app;
